f5b4956eeb4ad2e6a09e95c7d51ac6b3da25fd0c
IevaSkaiv <skaivydaite@gmail.com>
2022-05-24 10:52:14 +0300

    Create Driver class

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,7 +13,7 @@ import java.time.Duration;
 public class TestSelenium {
 
     @Test
-    public void testImputFieldDemoqa(){
+    public void testImputFieldDemoQA(){
         String fullName = "Ieva";
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
