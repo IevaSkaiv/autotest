7cd7c4cc6999df72dcfb3bb50aef493dfc39f703
IevaSkaiv <skaivydaite@gmail.com>
2022-05-24 13:47:41 +0300

    Refactor locators to separate class

--- src/test/java/pages/seleniumEasy/SimpleFormDemo.java
+++ src/test/java/pages/seleniumEasy/SimpleFormDemo.java
@@ -1,58 +1,51 @@
 package pages.seleniumEasy;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.Common;
+import pages.Locators;
 import utilities.Driver;
 
 import java.time.Duration;
 
 public class SimpleFormDemo {
 
-    public static void open(){
+    public static void open() {
         Common.openUrl("https://demo.seleniumeasy.com/basic-first-form-demo.html");
     }
 
-    public static void closeAd(){
+    public static void closeAd() {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='at-cv-lightbox-win']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(Locators.SeleniumEasy.frameAd));
 
-        Common.clickElement(By.xpath("//*[@id='at-cv-lightbox-close']"));
+        Common.clickElement(Locators.SeleniumEasy.buttonCloseAd);
     }
 
-    public static void enterMessage(String message){
-        By locator = By.xpath("//*[@id='user-message']");
-        Common.sendKeysToElement(locator, message);
+    public static void enterMessage(String message) {
+        Common.sendKeysToElement(Locators.SeleniumEasy.inputMessage, message);
     }
 
-    public static void clickShowMessageButton(){
-        By locator = By.xpath("//*[@id='get-input']/button");
-        Common.clickElement(locator);
+    public static void clickShowMessageButton() {
+        Common.clickElement(Locators.SeleniumEasy.buttonShowMessage);
     }
 
-    public static String readMessage(){
-        By locator = By.xpath("//*[@id='display']");
-        return Common.getElementText(locator);
+    public static String readMessage() {
+        return Common.getElementText(Locators.SeleniumEasy.fieldMessage);
     }
 
-    public static void enterValueA(String value){
-        By locator = By.xpath("//*[@id='sum1']");
-        Common.sendKeysToElement(locator, value);
+    public static void enterValueA(String value) {
+        Common.sendKeysToElement(Locators.SeleniumEasy.inputValueA, value);
     }
 
-    public static void enterValueB(String value){
-        By locator = By.xpath("//*[@id='sum2']");
-        Common.sendKeysToElement(locator, value);
+    public static void enterValueB(String value) {
+        Common.sendKeysToElement(Locators.SeleniumEasy.inputValueB, value);
     }
 
-    public static void clickGetTotalButton(){
-        By locator = By.xpath("//*[@id='gettotal']/button");
-        Common.clickElement(locator);
+    public static void clickGetTotalButton() {
+        Common.clickElement(Locators.SeleniumEasy.buttonGetTotal);
     }
 
-    public static String readTotal(){
-        By locator = By.xpath("//*[@id='displayvalue']");
-        return Common.getElementText(locator);
+    public static String readTotal() {
+        return Common.getElementText(Locators.SeleniumEasy.fieldTotal);
     }
 }
