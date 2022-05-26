0e31b33e648c9c7579d8d120f00855b8807acfea
IevaSkaiv <skaivydaite@gmail.com>
2022-05-26 11:17:42 +0300

    Add choreme options.

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
 
@@ -10,7 +11,11 @@ public class Driver {
 
     public static void setDriver(){
         WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("headless");
+//        options.addArguments("start-maximized");
+//        options.addArguments("window-size=5000,3000");
+        driver.set(new ChromeDriver(options));
     }
 
     public static WebDriver getDriver() {
